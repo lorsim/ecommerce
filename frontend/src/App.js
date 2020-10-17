463383bf91e57bf3ad1724bda66e62f07e2fd605
lorsim <lorsim96@gmail.com>
2020-10-17 17:16:47 +0530

    paypal integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
 
 const App=()=> {
   return (
@@ -19,14 +20,15 @@ const App=()=> {
     <Header/>
     <main className='py-3'>
       <Container>
-          <Route path='/shipping' component={ShippingScreen} exact/>
-          <Route path='/payment' component={PaymentScreen} exact/>
-          <Route path='/placeorder' component={PlaceOrderScreen} exact/>
-          <Route path='/login' component={LoginScreen} exact/>
-          <Route path='/register' component={RegisterScreen} exact/>
-          <Route path='/profile' component={ProfileScreen} exact/>
-          <Route path='/product/:id' component={ProductScreen} exact/>
-          <Route path='/cart/:id?' component={CartScreen} exact/>
+          <Route path='/order/:id' component={OrderScreen}/>
+          <Route path='/shipping' component={ShippingScreen}/>
+          <Route path='/payment' component={PaymentScreen}/>
+          <Route path='/placeorder' component={PlaceOrderScreen}/>
+          <Route path='/login' component={LoginScreen}/>
+          <Route path='/register' component={RegisterScreen}/>
+          <Route path='/profile' component={ProfileScreen}/>
+          <Route path='/product/:id' component={ProductScreen}/>
+          <Route path='/cart/:id?' component={CartScreen}/>
           <Route path='/' component={HomeScreen} exact/>
       </Container>
     </main>
