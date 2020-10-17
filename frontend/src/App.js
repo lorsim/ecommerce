213a59b55595fe4fc325e6e35524f63d27616aa3
lorsim <lorsim96@gmail.com>
2020-10-17 15:11:38 +0530

    cart and order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,10 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const App=()=> {
   return (
@@ -15,8 +19,12 @@ const App=()=> {
     <Header/>
     <main className='py-3'>
       <Container>
+          <Route path='/shipping' component={ShippingScreen} exact/>
+          <Route path='/payment' component={PaymentScreen} exact/>
+          <Route path='/placeorder' component={PlaceOrderScreen} exact/>
           <Route path='/login' component={LoginScreen} exact/>
           <Route path='/register' component={RegisterScreen} exact/>
+          <Route path='/profile' component={ProfileScreen} exact/>
           <Route path='/product/:id' component={ProductScreen} exact/>
           <Route path='/cart/:id?' component={CartScreen} exact/>
           <Route path='/' component={HomeScreen} exact/>
